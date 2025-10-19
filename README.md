fc495a89dd659a6c82184bd9407e760c0a2f6dab
ptem <emily.anible@gmail.com>
2025-10-19 17:57:24 -0400

    Initial commit + functionality.

---
+++ README.md
@@ -0,0 +1,3 @@
+# pulsar-punctual package
+
+Punctual is a language for live coding audio and visuals. Punctual-Pulsar integrates for the former into the latter.
---
+++ spec/pulsar-punctual-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PulsarPunctualView from '../lib/pulsar-punctual-view';
+
+describe('PulsarPunctualView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
